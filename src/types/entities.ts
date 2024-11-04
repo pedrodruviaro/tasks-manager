a03cf2e0c7dff7ba73fa786136e2b1edbf837a1a
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-04 06:38:04 -0300

    feat: :label: defining entities

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,7 +1,18 @@
 export interface Task {
   id: string
+  title: string
+  description?: string
+  completed: boolean
+  is_urgent?: boolean
+  project_id: string
+  due_date: Date
+  createdAt: Date
 }
 
 export interface Project {
   id: string
+  completed: boolean
+  title: string
+  description?: string
+  createdAt: Date
 }
